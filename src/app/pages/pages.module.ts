1c439ba60ee8c00af9ce9e638270f2c225f5c4ba
Hugo Ortiz <hugo.er.ortiz@gmail.com>
2018-02-13 22:04:00 -0600

    Avence del dia

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -6,12 +6,16 @@ import { PAGES_ROUTES } from './pages.routes';
 // Modulos
 import { SharedModule } from '../shared/shared.module';
 
+// Graficas
+import { ChartsModule } from 'ng2-charts';
+
 import { FormsModule } from '@angular/forms';
 import { PagesComponent } from './pages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
 import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
+import { GraficoDonaComponent } from '../components/grafico-dona/grafico-dona.component';
 
 @NgModule({
     declarations: [
@@ -19,6 +23,7 @@ import { IncrementadorComponent } from '../components/incrementador/incrementado
         DashboardComponent,
         ProgressComponent,
         IncrementadorComponent,
+        GraficoDonaComponent,
         Graficas1Component
     ],
     exports: [
@@ -29,6 +34,7 @@ import { IncrementadorComponent } from '../components/incrementador/incrementado
     imports: [
         SharedModule,
         FormsModule,
+        ChartsModule,
         PAGES_ROUTES
     ]
 })
