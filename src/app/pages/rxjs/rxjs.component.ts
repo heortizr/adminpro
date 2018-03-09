21d98b18b15799a91f671a56042b31f3882e115b
Hugo Ortiz <hugo.er.ortiz@gmail.com>
2018-03-08 23:51:06 -0600

    Avance

--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -1,18 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 // tslint:disable-next-line:import-blacklist
-import { Observable } from 'rxjs/Rx';
+import { Observable, Subscription } from 'rxjs/Rx';
 
 @Component({
   selector: 'app-rxjs',
   templateUrl: './rxjs.component.html',
   styles: []
 })
-export class RxjsComponent implements OnInit {
+export class RxjsComponent implements OnInit, OnDestroy {
+
+  subscription: Subscription;
 
   constructor() {
 
-
-    this.regresaObs.retry(2).subscribe(
+    this.subscription = this.regresaObservable().subscribe(
       numero => {
         console.log('subs: ', numero);
       }, error => {
@@ -27,27 +28,46 @@ export class RxjsComponent implements OnInit {
   ngOnInit() {
   }
 
-  regresaObs() {
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 
-    return new Observable( oberserver => {
-
+  regresaObservable(): Observable<any> {
+    return new Observable( observer => {
       let contador = 0;
-
-      let intervalo = setInterval( () => {
+      const intervalo = setInterval( () => {
 
         contador += 1;
-        oberserver.next(contador);
-        if ( contador === 3 ) {
-          clearInterval(intervalo);
-          oberserver.complete();
-        }
 
-        if ( contador === 2 ) {
-          oberserver.error();
-        }
+        const salida = {
+          valor: contador
+        };
 
-      }, 1000);
+        observer.next(salida);
+
+        // if ( contador === 3 ) {
+        //   clearInterval(intervalo);
+        //   observer.complete();
+        // }
+
+        // if ( contador === 2 ) {
+        //   observer.error();
+        // }
+
+      }, 500);
+    })
+    .retry(2)
+    .map( (respuesta: any) => {
+      return respuesta.valor;
+    })
+    .filter( (valor, index) => {
+      if ( (valor % 2) === 1 ) {
+        // impar
+        return true;
+      } else {
+        // par
+        return false;
+      }
     });
-
   }
 }
