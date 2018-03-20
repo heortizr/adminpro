eeda3875b19d246c164cc7cf628b99b4c9885dac
Hugo Ortiz <hugo.er.ortiz@gmail.com>
2018-03-19 23:01:10 -0600

    final de la seccion 13

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,7 @@ import { ServiceModule } from './services/service.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -27,6 +27,7 @@ import { FormsModule } from '@angular/forms';
     ServiceModule,
     PagesModule,
     FormsModule,
+    ReactiveFormsModule,
     APP_ROUTES
   ],
   providers: [],
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+import { UsuarioService } from '../services/service.index';
+import { Usuario } from '../models/usuario.model';
+import { element } from 'protractor';
 
 declare function init_plugins();
+declare const gapi: any;
 
 @Component({
   selector: 'app-login',
@@ -10,14 +15,70 @@ declare function init_plugins();
 })
 export class LoginComponent implements OnInit {
 
-  constructor( public router: Router ) { }
+  email: string;
+  recuerdame: boolean = false;
+  auth2: any;
+
+  constructor(
+    public router: Router,
+    public _usuarioService: UsuarioService
+  ) { }
 
   ngOnInit() {
+
     init_plugins();
+    this.googleInit();
+
+    this.email = localStorage.getItem('email') || '';
+
+    if (this.email.length > 1) {
+      this.recuerdame = true;
+    }
+
   }
 
-  ingresar() {
-    this.router.navigate([ '/dashboard' ]);
+  googleInit() {
+
+    gapi.load('auth2', () => {
+      this.auth2 = gapi.auth2.init({
+        client_id: '684708854038-tp79ionfpk7kug1f3h2vejpjgu7bm30g.apps.googleusercontent.com',
+        cookiepolicy: 'single_host_origin',
+        scope: 'profile email'
+      });
+
+      this.attachSignin(document.getElementById('btnGoogle'));
+
+    });
+
+  }
+
+
+  attachSignin( elemento ) {
+
+    this.auth2.attachClickHandler(elemento, {}, (googleUser: any) => {
+      // const profile = googleUser.getBasicProfile();
+
+      const token = googleUser.getAuthResponse().id_token;
+
+      this._usuarioService.loginGoogle(token)
+      .subscribe(correcto => {
+        this.router.navigate(['/dashboard']);
+      });
+    });
+  }
+
+  ingresar( forma: NgForm) {
+
+    if (forma.invalid) {
+      return;
+    }
+
+    const usuario = new Usuario(null, forma.value.email, forma.value.password);
+
+    this._usuarioService.login(usuario, forma.value.recuerdame)
+    .subscribe(correcto => {
+      this.router.navigate(['/dashboard']);
+    });
   }
 
 }
